d037a1beb7f0fd59e33271b5bd4c257db91afa89
Sakshi <115460967+sakshiii-13@users.noreply.github.com>
2024-03-05 13:24:35 +0530

    Update middleware.js

--- saar-first/middleware.js
+++ saar-first/middleware.js
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ['/'],
+  publicRoutes: ['/anyone-can-visit-this-route'],
   ignoredRoutes: ['/no-auth-in-this-route'],
 });
  
